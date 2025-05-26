c87725f544cb1cae0dff8861a4c7fe190a4524c7
AudenaK <audenakanaya.nf@gmail.com>
2025-05-26 16:29:23 +0700

    navbar user home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\homecontroller;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[homecontroller::class, 'index']);
+Route::get('/', [homecontroller::class, 'index']);
 
 Route::get('/hello', function () {
     return 'Hello Guys';
@@ -14,6 +14,6 @@ Route::get('/sidebar', function () {
     return view('components.sidebar');
 });
 
-Route::get('/test', function () {
-    return 'test';
+Route::get('/home', function () {
+    return view('user.index');
 });
