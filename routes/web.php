f03c1b51a4efc57fd9feea4ed2db2db581f37652
AudenaK <audenakanaya.nf@gmail.com>
2025-05-26 13:59:26 +0700

    cek

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/halo', function () {
-    return view('tak pernah ucap hello pergi kau melambai');
-});
