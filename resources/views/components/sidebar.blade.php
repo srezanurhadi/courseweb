a40dfc1bc0fc79886b1ebfec5b2d3a9b8705ed4b
Rnsaputra <rnsaputra12@gmail.com>
2025-05-26 15:28:57 +0700

    perbaikan file

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,53 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    @vite('resources/css/app.css')
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
-
-</head>
-
-<body>
-    <div class="h-screen flex">
-        <aside class="bg-gradient-to-b from-sky-600 to-indigo-900 w-54 flex flex-col space-y-1">
-            <div class="text-white text-3xl mt-4 pl-6 font-bold">R. DOSEN</div>
-            <a href="" class="m-6 mr-2 p-2 rounded-lg border-l-8 border-white bg-white/20 shadow-lg">
-                <i class="fas fa-gauge text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">Dashboard</span></a>
-            <div class="text-xs text-white pl-8 border-t-1 border-white/50">Area Admin</div>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-list text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">Content</span></a>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-book-open-reader  text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">Course</span></a>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-users-gear text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">User</span></a>
-                <div class="text-xs text-white pl-8 mt-4 border-t-1 border-white/50">Area Saya</div>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-sheet-plastic text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">My Content</span></a>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-book text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">My Course</span></a>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-user-gear text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">My Participant</span></a>
-            <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
-                <i class="fas fa-user text-white pl-1"></i>
-                <span class="text-white pl-1 font-semibold text-20">My Profile</span></a>
+<div class="h-screen flex sticky">
+    <aside class="bg-gradient-to-b from-sky-600 to-indigo-900 w-54 flex flex-col space-y-1">
+        <div class="text-white text-3xl mt-4 pl-6 font-bold">R. DOSEN</div>
+        <a href="" class="m-6 mr-2 p-2 rounded-lg border-l-8 border-white bg-white/20 shadow-lg">
+            <i class="fas fa-gauge text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">Dashboard</span></a>
+        <div class="text-xs text-white pl-8 border-t-1 border-white/50">Area Admin</div>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-list text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">Content</span></a>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-book-open-reader  text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">Course</span></a>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-users-gear text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">User</span></a>
+        <div class="text-xs text-white pl-8 mt-4 border-t-1 border-white/50">Area Saya</div>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-sheet-plastic text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">My Content</span></a>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-book text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">My Course</span></a>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-user-gear text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">My Participant</span></a>
+        <a href="" class="ml-6 p-2 rounded-lg border-l-8 border-white/30">
+            <i class="fas fa-user text-white pl-1"></i>
+            <span class="text-white pl-1 font-semibold text-20">My Profile</span></a>
 
 
-        </aside>
-    </div>
-</body>
-
-</html>
+    </aside>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    
-</head>
-<body>
-    
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <x-headcomponent></x-headcomponent>
+    </head>
+
+    <body>
+        <div class="flex flex-1">
+            <x-sidebar></x-sidebar>
+            <h1 class="font-bold text-9xl">Semua kurus</h1>
+        </div>
+    </body>
+
+    </html>
