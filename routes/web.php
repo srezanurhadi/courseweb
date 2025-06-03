31185e79abe3e5fd21ce01027e66d573e3761498
Rnsaputra <rnsaputra12@gmail.com>
2025-06-03 15:00:59 +0700

    perbaikan resource dan create

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [homecontroller::class, 'index']);
-Route::resource('/users',usersController::class);
+
+Route::prefix('/admin')->group(function () {
+    Route::get('/', [homecontroller::class, 'index']);
+    Route::resource('/users', usersController::class);
+});
+
+
+
 Route::get('/admin', function () {
     return 'Hello Guys';
 });
@@ -22,10 +29,6 @@ Route::get('/admincourseshow', function () {
     return view('admin.course.show');
 });
 
-//admin
-Route::get('/coba', function () {
-    return view('admin.users.index');
-});
 
 Route::get('/home', function () {
     return view('user.home');
