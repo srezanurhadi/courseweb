76d68b2d559b47eaf81122882a2dc243eae88260
AudenaK <audenakanaya.nf@gmail.com>
2025-06-04 15:34:40 +0700

    edit profile

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,6 @@ Route::prefix('/admin')->group(function () {
     Route::get('/anggotalihat', function () {
         return view('admin.myParticipant.show');
     });
-    
-    
 });
 
 
@@ -87,6 +85,14 @@ Route::prefix('/user')->name('user.')->group(function () {
     Route::get('/profile', function () {
         return view('user.myprofile.index');
     })->name('profile');
+
+    Route::get('/profile/edit', function () {
+        return view('user.myprofile.edit');
+    })->name('profile.edit');
+
+    Route::get('/profile/course/{id}', function ($id) {
+        return view('user.myprofile.detail', ['courseId' => $id]);
+    })->name('course.detail');
 });
 
 // AUDENA PUNYA
@@ -98,21 +104,3 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('user.register');
 });
-
-<<<<<<< HEAD
-Route::get('/userprofile', function () {
-    return view('user.myprofile.index');
-});
-
-Route::get('/userprofile/edit', function () {
-    return view('user.myprofile.edit');
-})->name('profile.edit');
-
-Route::get('/userprofile/course/{id}', function ($id) {
-    return view('user.myprofile.detail', ['courseId' => $id]);
-})->name('course.detail');
-=======
-// Route::get('/profile', function () {
-//     return view('user.myprofile.index');
-// });
->>>>>>> baa73dac1bc005f7ebf75029ed913e2afc748c1f
