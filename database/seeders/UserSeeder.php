b73886b43bb78cc7d88853594a6681e63c6c2cfc
BagusP1703 <bagusprayogo241@gmail.com>
2025-06-10 14:09:39 +0700

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,15 +4,38 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
+        // Admin
+        User::create([
+            'name' => 'admin',
+            'role' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+        ]);
+
+        // Author
+        User::create([
+            'name' => 'author',
+            'role' => 'author',
+            'email' => 'author@example.com',
+            'password' => Hash::make('author123'),
+        ]);
+
+        // Participant
+        User::create([
+            'name' => 'participant',
+            'role' => 'participant',
+            'email' => 'participant@example.com',
+            'password' => Hash::make('participant123'),
+        ]);
+    
         User::factory()->count(20)->create();
     }
 }
