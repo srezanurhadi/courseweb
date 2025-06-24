02ac4f8c2bfb8b85cf6696cabea0a7438175068e
srezanurhadi <srezanurhadi@gmail.com>
2025-06-24 14:31:30 +0700

    finalisasi content

--- app/Models/UploadedImage.php
+++ app/Models/UploadedImage.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo; // <-- Import
 
 class UploadedImage extends Model
@@ -12,13 +13,15 @@ class UploadedImage extends Model
         'filename',
         'path',
         'url',
+        'uploaded_by',
         'imageable_id',
         'imageable_type',
     ];
 
-    /**
-     * Dapatkan model parent yang memiliki gambar ini (Content, UserProfile, dll.).
-     */
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'uploaded_by');
+    }
     public function imageable(): MorphTo
     {
         return $this->morphTo();
